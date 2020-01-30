5d8d3e3baef03cc635565fd192dbaed9eed1f0e7
bananacabana <nwhays27@gmail.com>
2020-01-30 16:32:00 -0600

    attempt to fix the checkbox and make completed list. added comments to describe the javascript

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var $incompletedTasks = $("#incomplete-tasks");
 var $completedTasks = $("#completed-tasks");
 var $addButton = $("#add-button");
 
-//generate a new list item
+//This function allows the user to create a new list item
 var makeNewListItem = function(taskToAdd) {
 
   var $newListItem = $("<li></li>");
@@ -24,53 +24,43 @@ var makeNewListItem = function(taskToAdd) {
 
 //add new list item to To-Do list when add-button is clicked.
 $addButton.on( "click", function(){
-  //the value of the new task is passed as an argument in the makeNewListItem function; a new list item is returned and stored as the variable of listItemToAdd
   var listItemToAdd = makeNewListItem($newTask.val());
-  //append the new list item to the "To Do" list 
+
+  //this will append the new list item to the list
   $incompletedTasks.append(listItemToAdd);
-  //clears the text in the "add item" input after task is added
   $newTask.val("");
 })
 
-//remove a task from the "To-Do" list when the delete button is clicked
+//delete item from the list on click
 $incompletedTasks.on( "click", ".delete-button", function(){
   $(this).parent().remove();  
 })
 
-//remove a task from the "Completed" list when the delete button is clicked
+
 $completedTasks.on( "click", ".delete-button", function(){
   $(this).parent().remove();  
 })
 
-//function to edit tasks
+// tried for a function to edit the tasks.. doesnt work sometimes
 var editTask = function (list, input, label){
- //if the list has the class of edit mode
+ 
   if (list.hasClass("editMode")) {
-    //removes "editMode" class 
     list.removeClass("editMode");
-    //makes the label's text the same as the input value
     label.text(input.val());
   } else {
-    //adds the "editMode" class
     list.addClass("editMode");
-    //makes the input's value the same as the label's text 
     input.val(label.text());
   }
 }
 
-//when you click on the "edit button" in the "To-Do" list.... 
+
 $incompletedTasks.on( "click", ".edit-button", function(){
-  //the list of the clicked on task
   var $list = $(this).parent();  
-  //the input of the clicked on task
   var $input = $(this).prev();
-  //the label of the clicked on task
   var $label = $input.prev();
-  //the list, input, and label are passed to the editTask function
   editTask($list, $input, $label);
 })
 
-//when you click on the "edit button" in the "Completed" list.... 
 $completedTasks.on( "click", ".edit-button", function(){
   var $list = $(this).parent();  
   var $input = $(this).prev();
@@ -78,18 +68,3 @@ $completedTasks.on( "click", ".edit-button", function(){
   editTask($list, $input, $label);
 })
 
-//when you click on the checkbox in the "To-Do" list...
-$incompletedTasks.on( "click", "input.checkbox", function(){
- //the list item of the checkbox 
- var listItem = $(this).parent();
- //appends the list item to the "Completed" list
- $completedTasks.append(listItem);
-})
-
-//when you click on the checkbox in the "Completed" list...
-$completedTasks.on( "click", "input.checkbox", function(){
- //the list item of the checkbox 
- var listItem = $(this).parent();
- //appends the list item to the "To-Do" list
- $incompletedTasks.append(listItem);
-})
